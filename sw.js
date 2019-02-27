497bfb234a3fe2e599cc325097127816b5f06d28
kais sharma <kaissaroj@gmail.com>
2019-02-27 13:51:11 +0545

    first

---
+++ sw.js
@@ -0,0 +1,36 @@
+const version = 20190227;
+const cachePrefix = 'exceldb-';
+const staticCacheName = `${cachePrefix}static-${version}`;
+const expectedCaches = [staticCacheName];
+
+const cacheFiles = [
+    './',
+    'http://cdn.embedly.com/widgets/platform.js',
+    './res/bootstrap.min.css',
+    'https://fonts.googleapis.com/css?family=Inconsolata',
+    './files/file-spreadsheet.svg',
+    './files/magic.webp'
+];
+
+addEventListener('install', event => {
+    event.waitUntil((async () => {
+      const cache = await caches.open(staticCacheName);
+      await cache.addAll(cacheFiles);
+      self.skipWaiting();
+    })());
+  });
+  addEventListener('activate', event => {
+    event.waitUntil((async () => {
+      for (const cacheName of await caches.keys()) {
+        if (!cacheName.startsWith(cachePrefix)) continue;
+        if (!expectedCaches.includes(cacheName)) await caches.delete(cacheName);
+      }
+    })());
+  });
+  
+  addEventListener('fetch', event => {
+    const url = new URL(event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(r => r || fetch(event.request))
+    );
+  });
